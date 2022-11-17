02c8cb1ed39c93eb4c507bc10fc62995d1415f63
Mark Gould <mgould@spindlemedia.com>
2022-11-17 11:16:04 -0600

    Initial value types

---
+++ src/BeerSender/BeerSender.Console/Program.cs
@@ -0,0 +1,5 @@
+﻿using BeerSender.Domain;
+
+var label1 = new ShippingLabel("a", Carrier.DHL);
+
+Console.WriteLine();
---
+++ src/BeerSender/BeerSender.Domain/ValueTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace BeerSender.Domain;
+
+public record ShippingLabel(string ShippingCode, Carrier Carrier)
+{
+    public bool IsValid()
+    {
+        return ShippingCode.Length > 10;
+    }
+}
+
+public enum Carrier
+{
+    UPS,
+    FedEx,
+    DHL
+}
+
+public record BeerBottle(string Brewery, string Name);
